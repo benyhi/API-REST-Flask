25240cda5526101e3425c0684a5899fbf77d8db6
benyhi <sosabenjaminmartin09@gmail.com>
2024-10-21 14:10:22 -0300

    .env y modif en app

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
 
+load_dotenv()
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+pymysql://root@localhost/efi1'
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'secret_key'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
+ma = Marshmallow(app)
 
 import models
 
